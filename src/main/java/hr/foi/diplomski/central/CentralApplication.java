a12a9bced23da767e83a9fd95bb0f628056f4711
Pavao Vlahovic <pvlahovic1@foi.hr>
2019-09-09 22:55:36 +0200

    Time zone is set to Europe/Zagreb

--- src/main/java/hr/foi/diplomski/central/CentralApplication.java
+++ src/main/java/hr/foi/diplomski/central/CentralApplication.java
@@ -3,12 +3,20 @@ package hr.foi.diplomski.central;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CentralApplication {
 
 	//Angular client
 	public static void main(String[] args) {
 		SpringApplication.run(CentralApplication.class, args);
+	}
+
+	@PostConstruct
+	void configOnStart() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Zagreb"));
 	}
 
 }
